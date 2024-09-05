c6d4f6c273bb57a7680d1d467aa90b822ce9116a
yanxl <923932093@qq.com>
2024-09-05 17:50:27 +0800

    add Speedhack

---
+++ Speedhack/Speedhack/dllmain.cpp
@@ -0,0 +1,21 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include "speedhack.h"
+
+DWORD WINAPI MainThread(LPVOID lpParam)
+{
+    Speedhack::Setup();
+    Speedhack::SetSpeed(5.0); //slows down to half speed
+
+    return 0;
+}
+
+BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+    if (dwReason == DLL_PROCESS_ATTACH)
+    {
+        CreateThread(0, 0x1000, &MainThread, NULL, 0, NULL);
+    }
+
+    return TRUE;
+}

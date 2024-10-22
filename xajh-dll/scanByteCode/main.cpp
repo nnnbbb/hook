3023444358c827945759ace2799cf9b587357244
nnnbbb <923932093@qq.com>
2024-10-22 15:22:17 +0800

    scanByteCode

---
+++ xajh-dll/scanByteCode/main.cpp
@@ -0,0 +1,92 @@
+﻿#include <Windows.h>
+#include <string>
+#include <iostream>
+#include <tlhelp32.h>
+#include <processsnapshot.h>
+
+DWORD GetPidByName(const WCHAR* name) {
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    PROCESSENTRY32 pe32 = {sizeof(PROCESSENTRY32)};
+    DWORD pid = 0;
+
+    if (Process32First(snapshot, &pe32)) {
+        do {
+            WCHAR* szName = pe32.szExeFile;
+            // printf("szName = %ls\n", szName);
+            if (wcscmp(szName, name) == 0) {
+                pid = pe32.th32ProcessID;
+                break;
+            }
+        } while (Process32Next(snapshot, &pe32));
+    }
+    CloseHandle(snapshot);
+    return pid;
+}
+
+std::string removeSpaces(std::string& input) {
+    input.erase(std::remove(input.begin(), input.end(), ' '), input.end());
+    return input;
+}
+
+
+BOOL ByteToChar(BYTE* byteCode, char* strCode, int codeLen) {
+    for (int i = 0; i < codeLen; i++) {
+        wsprintfA(&strCode[i * 2], "%02X", byteCode[i]);
+    }
+    return TRUE;
+}
+
+BOOL CmpStrCode(char* code, char* readStr, int len) {
+    for (int i = 0; i < len; i++) {
+        if (code[i] == '?') {
+            continue;
+        }
+        if (code[i] != readStr[i]) {
+            return FALSE;
+        }
+    }
+    return TRUE;
+}
+
+BOOL ScanGameCode(
+    HANDLE hProcess,
+    DWORD beginAddr,
+    DWORD endAddr,
+    char* code,
+    DWORD codeLen,
+    DWORD& retAddr
+) {
+    const size_t size = 0x1000;
+    BYTE* readCode = new BYTE[size];
+    for (DWORD readAddr = beginAddr; readAddr <= endAddr - size; readAddr += (size - codeLen)) {
+        memset(readCode, 0, size);
+        char strCode[size * 2 + 1] = {0};
+        BOOL retRead = ReadProcessMemory(hProcess, (LPVOID)readAddr, readCode, size, NULL);
+        if (retRead == 0) {
+            continue;
+        }
+        ByteToChar(readCode, strCode, size);
+        for (size_t i = 0; i < size * 2 + 1 - codeLen; i++) {
+            BOOL retCmp = CmpStrCode(code, &strCode[i], codeLen);
+            if (retCmp == TRUE) {
+                retAddr = readAddr + i / 2;
+                printf("起始地址为:0x%X\n", retAddr);
+            }
+        }
+    }
+    return TRUE;
+}
+
+
+int main() {
+    DWORD pid = GetPidByName(L"xajh.exe");
+    printf("pid -> %d\n", pid);
+    std::string code = "C6 84 24 CC 00 00 00 05 E8 52 42 18 00";
+    removeSpaces(code);
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, (DWORD)pid);
+    DWORD addr = 0;
+    ScanGameCode(hProcess, 0x00AA94FF, 0x00BA9600, (char*)code.c_str(), code.size(), addr);
+    printf("addr -> 0x%X\n", addr);
+    system("pause");
+    return 0;
+}
